8af4195f035c1e79bc7aafdfbb139d7f5e990493
Takashi Yoshida <takashi@takashiyoshida.org>
2013-10-26 06:14:09 -0700

    Switch to use dietermeyer/puppet-osx

--- modules/people/manifests/takashiyoshida.pp
+++ modules/people/manifests/takashiyoshida.pp
@@ -1,8 +1,5 @@
 class people::takashiyoshida {
   # Firewall configuration
-  # jfelchner/puppet-osx (tag 1.2.3) has a vastly different syntax from master version
-  # The future version of jfelchner/puppet-osx will be incompatible with the following
-
-  class { "osx::firewall": enabled => true }
-  class { "osx::firewall::stealth_mode": enabled => true }
+  class { "osx::firewall": ensure => 'present' }
+  #class { "osx::firewall::stealth_mode": enabled => true }
 }
