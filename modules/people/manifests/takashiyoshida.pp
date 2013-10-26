de7bdecb65c451db9ab2dadb55c314ec13f0a72d
Takashi Yoshida <takashi@takashiyoshida.org>
2013-10-26 06:22:34 -0700

    Enable stealth mode in firewall

--- modules/people/manifests/takashiyoshida.pp
+++ modules/people/manifests/takashiyoshida.pp
@@ -1,5 +1,5 @@
 class people::takashiyoshida {
   # Firewall configuration
   class { "osx::firewall": ensure => 'present' }
-  #class { "osx::firewall::stealth_mode": enabled => true }
+  class { "osx::firewall::stealth_mode": ensure => 'present' }
 }
