5d2272338c45d981d1b021d89ddb24050693e1d5
Takashi Yoshida <takashi@takashiyoshida.org>
2013-10-28 07:20:43 -0700

    Move homebrew packages to homebrew.pp

--- modules/people/manifests/takashiyoshida.pp
+++ modules/people/manifests/takashiyoshida.pp
@@ -1,36 +1,6 @@
 class people::takashiyoshida {
   include people::takashiyoshida::osx_settings
   include people::takashiyoshida::applications
-
-  # Homebrew
-  homebrew::tap { "homebrew/binary": }
-
-  package {
-    "emacs": ;
-    "ffmpeg": ;
-    "mercurial": ;
-    "p7zip": ;
-    "the_silver_searcher": ;
-    "spark": ;
-    "tig": ;
-    "tmux": ;
-    "unrar": ;
-    "vim": ;
-    "youtube-dl": ;
-    "zsh":
-      install_options => [ "--disable-etcdir" ]
-  }
-
-  file_line { 'Add zsh to /etc/shells':
-    path => '/etc/shells',
-    line => "${boxen::config::homebrewdir}/bin/zsh",
-    require => Package['zsh'],
-  }
-
-  osx_chsh { $::luser:
-    shell => "${boxen::config::homebrewdir}/bin/zsh",
-    require => Package['zsh'],
-  }
-
+  include people::takashiyoshida::homebrew
   include people::takashiyoshida::repositories
 }
---
+++ modules/people/manifests/takashiyoshida/homebrew.pp
@@ -0,0 +1,31 @@
+class people::takashiyoshida::homebrew {
+    # Homebrew
+  homebrew::tap { "homebrew/binary": }
+
+  package {
+    "emacs": ;
+    "ffmpeg": ;
+    "mercurial": ;
+    "p7zip": ;
+    "the_silver_searcher": ;
+    "spark": ;
+    "tig": ;
+    "tmux": ;
+    "unrar": ;
+    "vim": ;
+    "youtube-dl": ;
+    "zsh":
+      install_options => [ "--disable-etcdir" ]
+  }
+
+  file_line { 'Add zsh to /etc/shells':
+    path => '/etc/shells',
+    line => "${boxen::config::homebrewdir}/bin/zsh",
+    require => Package['zsh'],
+  }
+
+  osx_chsh { $::luser:
+    shell => "${boxen::config::homebrewdir}/bin/zsh",
+    require => Package['zsh'],
+  }
+}
