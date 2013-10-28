13d94098b71d9743419f2d44e4375863f0f17fa2
Takashi Yoshida <takashi@takashiyoshida.org>
2013-10-28 06:27:25 -0700

    Move OS X configurations to osx_settings.pp

---
+++ modules/people/manifests/takashiyoshida/osx_settings.pp
@@ -0,0 +1,30 @@
+class people::takashiyoshida::osx_settings {
+  # Firewall configuration
+  class { "osx::firewall": ensure => 'present' }
+  class { "osx::firewall::stealth_mode": ensure => 'present' }
+
+  # Dock settings
+  class { "osx::dock::orientation": orientation => 'left' }
+  class { "osx::dock::magnification": ensure => 'present' }
+
+  # Finder settings
+  class { "osx::finder::status_bar": ensure => 'present' }
+  class { "osx::finder::quicklook_text_selection": ensure => 'present' }
+
+  class { "osx::finder::file_extensions":
+    display_all => false,
+    warn_on_change => true,
+  }
+
+  class { "osx::finder::default_location": location => 'home directory' }
+
+  # Miscellaneous
+  class { "osx::system::crash_reporter": ensure => 'present' }
+  class { "osx::dialogs::auto_expanding_save_dialog": ensure => 'present' }
+
+  class { "osx::keyboard::default_to_alternate_function_keys": ensure => 'present' }
+  class { "osx::network::dsstores": ensure => 'present' }
+
+  class { "osx::universal_access::assistive_device_access": ensure => 'present' }
+  class { "osx::windows::miniaturize_on_double_click": ensure => 'present' }
+}
