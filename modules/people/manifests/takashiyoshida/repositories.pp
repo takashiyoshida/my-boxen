31c7ed7e9d5732ead1ace02cac1af1a367a20793
Takashi Yoshida <takashi@takashiyoshida.org>
2013-12-21 12:46:27 +0800

    Add osx-vm-templates to repositories.pp

--- modules/people/manifests/takashiyoshida/repositories.pp
+++ modules/people/manifests/takashiyoshida/repositories.pp
@@ -12,68 +12,70 @@ class people::takashiyoshida::repositories {
     require => File[$projects],
   }
 
-  $applescript = "${projects}/applescript"
-  $dotfiles = "${projects}/dotfiles"
-  $oh_my_zsh = "${projects}/oh-my-zsh"
-  $solarized = "${projects}/solarized"
-  $term_solarized = "${projects}/osx-terminal.app-colors-solarized"
-  $ql_markdown = "${projects}/qlmarkdown"
-  $prelude = "${projects}/prelude"
+  $applescript        = "${projects}/applescript"
+  $dotfiles           = "${projects}/dotfiles"
+  $oh_my_zsh          = "${projects}/oh-my-zsh"
+  $solarized          = "${projects}/solarized"
+  $term_solarized     = "${projects}/osx-terminal.app-colors-solarized"
+  $ql_markdown        = "${projects}/qlmarkdown"
+  $prelude            = "${projects}/prelude"
 
-  $puppet_dropbox = "${projects}/puppet-dropbox"
+  $puppet_dropbox     = "${projects}/puppet-dropbox"
   $puppet_fastscripts = "${projects}/puppet-fastscripts"
-  $puppet_launchbar = "${projects}/puppet-launchbar"
-  $puppet_spamsieve = "${projects}/puppet-spamsieve"
-  $puppet_transmit = "${projects}/puppet-transmit"
-  $puppet_undercover = "${projects}/puppet-undercover"
+  $puppet_launchbar   = "${projects}/puppet-launchbar"
+  $puppet_spamsieve   = "${projects}/puppet-spamsieve"
+  $puppet_transmit    = "${projects}/puppet-transmit"
+  $puppet_undercover  = "${projects}/puppet-undercover"
+
+  $osx_vm_templates   = "${projects}/osx-vm-templates"
 
   repository { $applescript:
-    source => 'takashiyoshida/applescript',
+    source  => 'takashiyoshida/applescript',
     require => File[$projects],
   }
 
   repository { $dotfiles:
-    source => 'takashiyoshida/dotfiles',
+    source  => 'takashiyoshida/dotfiles',
     require => File[$projects],
   }
 
   repository { $oh_my_zsh:
-    source => 'takashiyoshida/oh-my-zsh',
+    source  => 'takashiyoshida/oh-my-zsh',
     require => File[$projects],
   }
 
   repository { $solarized:
-    source => 'altercation/solarized',
+    source  => 'altercation/solarized',
     require => File[$projects],
   }
 
   repository { $term_solarized:
-    source => 'tomislav/osx-terminal.app-colors-solarized',
+    source  => 'tomislav/osx-terminal.app-colors-solarized',
     require => File[$projects],
   }
 
   repository { $ql_markdown:
-    source => 'toland/qlmarkdown',
+    source  => 'toland/qlmarkdown',
     require => File[$projects],
   }
 
   repository { $prelude:
-    source => 'bbatsov/prelude',
+    source  => 'bbatsov/prelude',
     require => File[$projects],
   }
 
   repository { $puppet_dropbox:
-    source => 'takashiyoshida/puppet-dropbox',
+    source  => 'takashiyoshida/puppet-dropbox',
     require => File[$projects],
   }
 
   repository { $puppet_fastscripts:
-    source => 'takashiyoshida/puppet-fastscripts',
+    source  => 'takashiyoshida/puppet-fastscripts',
     require => File[$projects],
   }
 
   repository { $puppet_launchbar:
-    source => 'takashiyoshida/puppet-launchbar',
+    source  => 'takashiyoshida/puppet-launchbar',
     require => File[$projects],
   }
 
@@ -89,6 +91,11 @@ class people::takashiyoshida::repositories {
 
   repository { $puppet_undercover:
     source => 'takashiyoshida/puppet-undercover',
+    require => File[$projects],
+  }
+
+  repository { $osx_vm_templates:
+    source => 'timsutton/osx-vm-templates',
     require => File[$projects],
   }
 }
