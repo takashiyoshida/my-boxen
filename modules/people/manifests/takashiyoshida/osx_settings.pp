7d6df08d9f3986dfc5cbdb1f6f64afa7c70babd3
Takashi Yoshida <takashi@takashiyoshida.org>
2015-07-03 21:52:38 +0800

    Remove comments

--- modules/people/manifests/takashiyoshida/osx_settings.pp
+++ modules/people/manifests/takashiyoshida/osx_settings.pp
@@ -1,29 +1,9 @@
 class people::takashiyoshida::osx_settings {
-  # Firewall configuration
-  #class { "osx::firewall": ensure => 'present' }
-  #class { "osx::firewall::stealth_mode": ensure => 'present' }
-
   # Dock settings
-  #class { "osx::dock::orientation": orientation => 'left' }
-  #class { "osx::dock::magnification": ensure => 'present' }
   class { 'osx::dock::position': position => 'left' }
   include osx::dock::magnification
 
   # Finder settings
-  #class { "osx::finder::status_bar": ensure => 'present' }
-  #class { "osx::finder::quicklook_text_selection": ensure => 'present' }
-
-  #class { "osx::finder::file_extensions":
-  #  display_all => false,
-  #  warn_on_change => true,
-  #}
-
-  #class { "osx::finder::default_location": location => 'home directory' }
-
-  # Miscellaneous
-  #class { "osx::system::crash_reporter": ensure => 'present' }
-  #class { "osx::dialogs::auto_expanding_save_dialog": ensure => 'present' }
-
   include osx::finder::show_external_hard_drives_on_desktop
   include osx::finder::show_mounted_servers_on_desktop
   include osx::finder::show_removable_media_on_desktop
@@ -34,15 +14,11 @@ class people::takashiyoshida::osx_settings {
   include osx::finder::show_warning_before_emptying_trash
   include osx::finder::show_warning_before_changing_an_extension
 
+  # Miscellaneous
   include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
 
-  #class { "osx::keyboard::default_to_alternate_function_keys": ensure => 'present' }
   include osx::global::enable_standard_function_keys
-  #class { "osx::network::dsstores": ensure => 'present' }
   include osx::no_network_dsstores
   include osx::keyboard::capslock_to_control
-
-  #class { "osx::universal_access::assistive_device_access": ensure => 'present' }
-  #class { "osx::windows::miniaturize_on_double_click": ensure => 'present' }
 }
