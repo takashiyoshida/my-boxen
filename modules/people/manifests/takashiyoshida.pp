2f8ebb546b679b199b5f1d14171961c7a3f66651
Takashi Yoshida <takashi@Takashis-MacBook-Pro.local>
2015-07-07 14:19:11 +0800

    Do not install OS X apps via brewcask

--- modules/people/manifests/takashiyoshida.pp
+++ modules/people/manifests/takashiyoshida.pp
@@ -1,6 +1,6 @@
 class people::takashiyoshida {
   include people::takashiyoshida::osx_settings
-  include people::takashiyoshida::applications
+#  include people::takashiyoshida::applications
   include people::takashiyoshida::homebrew
   include people::takashiyoshida::repositories
 }
--- modules/people/manifests/takashiyoshida/applications.pp
+++ modules/people/manifests/takashiyoshida/applications.pp
@@ -1,115 +1,115 @@
-class people::takashiyoshida::applications {
-  # Applications  
-  include brewcask
+#class people::takashiyoshida::applications {
+#  Applications  
+#  include brewcask
   
 #  package { 'adobe-creative-cloud':
 #    provider => 'brewcask',
 #    install_options => '--appdir=/Applications',
 #  }
 
-  package { 'arduino':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'arduino':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'bbedit':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'bbedit':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'cocktail':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'cocktail':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'cornerstone':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'cornerstone':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
   
-  package { 'data-rescue':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'data-rescue':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
   
-  package { 'dropbox':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'dropbox':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
   
-  package { 'fastscripts':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'fastscripts':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'flux':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'flux':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'google-chrome':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'google-chrome':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'handbrake':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'handbrake':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
   
-  package { 'launchbar':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'launchbar':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'pdfpen':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'pdfpen':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'spamsieve':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'spamsieve':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'superduper':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'superduper':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'textexpander':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'textexpander':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'todoist':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'todoist':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
 #  package { 'tower':
 #    provider => 'brewcask',
 #    install_options => '--appdir=/Applications',
 #  }
 
-  package { 'transmission':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'transmission':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'transmit':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'transmit':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
-  package { 'vlc':
-    provider => 'brewcask',
-    install_options => '--appdir=/Applications',
-  }
+#  package { 'vlc':
+#    provider => 'brewcask',
+#    install_options => '--appdir=/Applications',
+#  }
 
 #  package { 'vmware-fusion':
 #    provider => 'brewcask',
 #    install_options => '--appdir=/Applications',
 #  }
 
-}
+#}
